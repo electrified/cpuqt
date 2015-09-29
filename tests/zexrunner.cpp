9b12b19dea2c5b14c813a21b8d3698d4a30f34f4
ed <ebwibble@hotmail.com>
2015-09-29 08:33:37 +0100

    remove rogue i++

--- tests/zexrunner.cpp
+++ tests/zexrunner.cpp
@@ -20,7 +20,7 @@ int main(int argc, char** argv)
     boost::timer::auto_cpu_timer t;
 //     while(true) {
      for (std::uint64_t i = 0; i < 100000000; i++) {
-        ++i;
+//         ++i;
         if (i % 10000000 == 0) {
             std::cout << i << std::endl;
         }
